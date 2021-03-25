7a56c035976f640e01e02f0bb2c6df382b6288a3
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-03-25 18:39:21 +0800

    update to clean faiss v1.7.0

--- core/src/index/thirdparty/faiss/tests/test_binary_flat.cpp
+++ core/src/index/thirdparty/faiss/tests/test_binary_flat.cpp
@@ -23,8 +23,6 @@ TEST(BinaryFlat, accuracy) {
   // make the index object and train it
   faiss::IndexBinaryFlat index(d);
 
-  srand(35);
-
   std::vector<uint8_t> database(nb * (d / 8));
   for (size_t i = 0; i < nb * (d / 8); i++) {
     database[i] = rand() % 0x100;
@@ -51,9 +49,9 @@ TEST(BinaryFlat, accuracy) {
 
     for (size_t i = 0; i < nq; ++i) {
       faiss::HammingComputer8 hc(queries.data() + i * (d / 8), d / 8);
-      hamdis_t dist_min = hc.compute(database.data());
+      hamdis_t dist_min = hc.hamming(database.data());
       for (size_t j = 1; j < nb; ++j) {
-        hamdis_t dist = hc.compute(database.data() + j * (d / 8));
+        hamdis_t dist = hc.hamming(database.data() + j * (d / 8));
         if (dist < dist_min) {
           dist_min = dist;
         }
--- core/src/index/thirdparty/faiss/tutorial/cpp/3-IVFPQ.cpp
+++ core/src/index/thirdparty/faiss/tutorial/cpp/3-IVFPQ.cpp
@@ -7,38 +7,35 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <random>
 
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFPQ.h>
-#include "../../utils/ConcurrentBitset.h"
 
+using idx_t = faiss::Index::idx_t;
 
 int main() {
     int d = 64;                            // dimension
     int nb = 100000;                       // database size
-    int nq = 10;//10000;                        // nb of queries
-    faiss::ConcurrentBitsetPtr bitset = std::make_shared<faiss::ConcurrentBitset>(nb);
+    int nq = 10000;                        // nb of queries
+
+    std::mt19937 rng;
+    std::uniform_real_distribution<> distrib;
 
     float *xb = new float[d * nb];
     float *xq = new float[d * nq];
 
     for(int i = 0; i < nb; i++) {
         for(int j = 0; j < d; j++)
-            xb[d * i + j] = drand48();
+            xb[d * i + j] = distrib(rng);
         xb[d * i] += i / 1000.;
     }
 
-    srand((unsigned)time(NULL));
-    printf("delete ids: \n");
     for(int i = 0; i < nq; i++) {
-        auto tmp = rand()%nb;
-        bitset->set(tmp);
-        printf("%d ", tmp);
         for(int j = 0; j < d; j++)
-            xq[d * i + j] = xb[d * tmp + j];
-//        xq[d * i] += i / 1000.;
+            xq[d * i + j] = distrib(rng);
+        xq[d * i] += i / 1000.;
     }
-    printf("\n");
 
 
     int nlist = 100;
@@ -46,13 +43,12 @@ int main() {
     int m = 8;                             // bytes per vector
     faiss::IndexFlatL2 quantizer(d);       // the other index
     faiss::IndexIVFPQ index(&quantizer, d, nlist, m, 8);
-    // here we specify METRIC_L2, by default it performs inner-product search
+
     index.train(nb, xb);
     index.add(nb, xb);
 
-    printf("------------sanity check----------------\n");
     {       // sanity check
-        long *I = new long[k * 5];
+        idx_t *I = new idx_t[k * 5];
         float *D = new float[k * 5];
 
         index.search(5, xb, k, D, I);
@@ -60,7 +56,7 @@ int main() {
         printf("I=\n");
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
+                printf("%5zd ", I[i * k + j]);
             printf("\n");
         }
 
@@ -75,37 +71,17 @@ int main() {
         delete [] D;
     }
 
-    printf("---------------search xq-------------\n");
     {       // search xq
-        long *I = new long[k * nq];
+        idx_t *I = new idx_t[k * nq];
         float *D = new float[k * nq];
 
         index.nprobe = 10;
         index.search(nq, xq, k, D, I);
 
         printf("I=\n");
-        for(int i = 0; i < nq; i++) {
+        for(int i = nq - 5; i < nq; i++) {
             for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
-            printf("\n");
-        }
-
-        delete [] I;
-        delete [] D;
-    }
-
-    printf("----------------search xq with delete------------\n");
-    {       // search xq with delete
-        long *I = new long[k * nq];
-        float *D = new float[k * nq];
-
-        index.nprobe = 10;
-        index.search(nq, xq, k, D, I, bitset);
-
-        printf("I=\n");
-        for(int i = 0; i < nq; i++) {
-            for(int j = 0; j < k; j++)
-                printf("%5ld ", I[i * k + j]);
+                printf("%5zd ", I[i * k + j]);
             printf("\n");
         }
 
