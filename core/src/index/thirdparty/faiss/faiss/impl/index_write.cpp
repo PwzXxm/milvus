91a6277edaff93a533c7e1f8e5ef24ff5ad7346f
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2021-04-08 16:53:26 +0800

    add readonly IVF in index_write.cpp

--- core/src/index/thirdparty/faiss/faiss/impl/index_write.cpp
+++ core/src/index/thirdparty/faiss/faiss/impl/index_write.cpp
@@ -210,7 +210,24 @@ void write_InvertedLists (const InvertedLists *ils, IOWriter *f) {
                 WRITEANDCHECK (ails->ids[i].data(), n);
             }
         }
-
+    } else if (const auto & oa =
+            dynamic_cast<const ReadOnlyArrayInvertedLists *>(ils)) {
+        uint32_t h = fourcc("iloa");
+        WRITE1 (h);
+        WRITE1 (oa->nlist);
+        WRITE1 (oa->code_size);
+        WRITEVECTOR(oa->readonly_length);
+#ifdef USE_CPU
+        size_t n = oa->readonly_ids.size();
+        WRITE1(n);
+        WRITEANDCHECK(oa->readonly_ids.data(), n);
+        WRITEANDCHECK(oa->readonly_codes.data(), n * oa->code_size);
+#else
+        size_t n = oa->pin_readonly_ids->size() / sizeof(InvertedLists::idx_t);
+        WRITE1(n);
+        WRITEANDCHECK((InvertedLists::idx_t *) oa->pin_readonly_ids->data, n);
+        WRITEANDCHECK((uint8_t *) oa->pin_readonly_codes->data, n * oa->code_size);
+#endif
     } else {
         InvertedListsIOHook::lookup_classname(
                 typeid(*ils).name())->write(ils, f);
